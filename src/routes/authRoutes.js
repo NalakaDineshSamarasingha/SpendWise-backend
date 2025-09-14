80b7038e90b610c4e9fe85cb567885c0c85c9c54
NalakaDineshSamarasingha <nalakadinesh97@gmail.com>
2025-09-14 23:05:45 +0530

    add yaml fiile

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -20,8 +20,11 @@ router.get(
       process.env.JWT_SECRET,
       { expiresIn: "7d" }
     );
-    // Send token to client (in real app, redirect with token or set cookie)
-    res.json({ token });
+    // Redirect to frontend with token in URL
+    console.log("Fallback receive");
+    const redirectUrl = req.query.redirect_uri || "http://localhost:8081"; // fallback to Expo dev URL
+    console.log(redirectUrl);
+    res.redirect(`${redirectUrl}?token=${token}`);
   }
 );
 
